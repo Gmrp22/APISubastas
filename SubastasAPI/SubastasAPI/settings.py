c64047ae8f9c10575f9cba672b3b7a3abd139cfb
Gilda <gmrp22@hotmail.com>
2020-11-17 13:16:51 -0600

    Mysql

--- SubastasAPI/SubastasAPI/settings.py
+++ SubastasAPI/SubastasAPI/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.Oferta',
+    'apps.Producto',
+    'apps.Subasta',
+    'apps.Usuario',
+    'apps.Venta',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -74,10 +81,17 @@ WSGI_APPLICATION = 'SubastasAPI.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'subasta',
+        'USER' : 'subasta',
+        'PASSWORD' :'subasta',
+        'HOST' :'localhost',
+        'PORT' :'3306',
+
     }
+}
 }
 
 
