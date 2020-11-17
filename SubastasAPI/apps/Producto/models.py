5bf1bec3cdbf286a1d01428c2ada5b0c2d0bf97d
Gilda <gmrp22@hotmail.com>
2020-11-17 13:31:11 -0600

    Creacion de modelos Oferta,Subasta yProducto

--- SubastasAPI/apps/Producto/models.py
+++ SubastasAPI/apps/Producto/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+#from apps.Usuario.models import Usuario
 # Create your models here.
+class Producto(models.Model):
+    """ Modelo del producto"""
+
+    Nombre = models.CharField(max_length=200)
+    Codigo = models.CharField(max_length=200, default="")
+    Estado = models.CharField(max_length=200, default='Espera', blank=True)
+    #Vendedor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.Codigo)
--- SubastasAPI/apps/Venta/models.py
+++ SubastasAPI/apps/Venta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from apps.Subasta.models import Subasta
+from apps.Oferta.models import Oferta
+from apps.Producto.models import Producto
 # Create your models here.
+class Venta(models.Model):
+    """ Modelo de venta"""
+    Fecha = models.DateField()
+    #Vendedor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    #Total = models.OneToOneField(Oferta, on_delete=models.CASCADE)
+    #Subasta = models.OneToOneField(Subasta, on_delete=models.CASCADE)
+    #Producto = models.OneToOneField(Producto, on_delete=models.CASCADE)
