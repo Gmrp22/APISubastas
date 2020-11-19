d26845ad1311d7614040fcc135866f3fac25a93e
Gilda <gmrp22@hotmail.com>
2020-11-18 18:36:05 -0600

    Permisos terminados

--- SubastasAPI/apps/Subasta/permissions.py
+++ SubastasAPI/apps/Subasta/permissions.py
@@ -4,7 +4,7 @@ class IsOwnerOrReadOnly(BasePermission):
     """
     Permitir que solo los dueños de la subasta puedan modificar
     """
-    message= 'No tiene permisos para editar esta subastas'
+    message= 'No tiene permisos para editar esta subasta'
     metodos=['PUT', 'DELETE']
     def has_object_permission(self, request, view, obj):
         if request.method in self.metodos and  obj.Nombre_Producto.Vendedor == request.user:
